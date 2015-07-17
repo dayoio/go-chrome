2133b626028f9225bfa430fbf67b4e541a53fdd5
Siuzuk Zan <minoscc+github@gmail.com>
2015-07-17 12:19:59 +0800

    居中視頻，隱藏右邊列表

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,16 +1,31 @@
+(function (document) {
+  var f = document.getElementById('flash');
+  var c = document.getElementsByClassName('act_shares')[0].childNodes[1];
+  var o = document.getElementsByClassName('code-input fl')[1];
+  f.data = '';
 
-var f = document.getElementById('flash');
-var c = document.getElementsByClassName('act_shares')[0].childNodes[1];
-var o = document.getElementsByClassName('code-input fl')[1];
-f.data = '';
+  var css = '.videoWindow_cover { width:0; height:0; display:none;} .zongyi-rightList { display:none;} body.qypage-1180 .play-zongyiWrap .zongyiVideo, body.qypage-1440 .play-zongyiWrap .zongyiVideo, .play-zongyiWrap .zongyiVideo { float: none; margin: 0 auto;}';
 
-var s = setInterval(function () {
-  c.click();
-  console.log(o.value);
-  if (o.value !== "") {
-    f.data = o.value;
-    c.click();
-    document.getElementsByClassName('videoWindow_cover')[0].style = "width:0;height:0;display:none;"
-    clearInterval(s);
+  var style = document.createElement('style');
+  style.type = 'text/css';
+
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
   }
-}, 1000);
+
+  document.head.appendChild(style);
+
+  var s = setInterval(function () {
+    c.click();
+    console.log(o.value);
+    if (o.value !== "") {
+      f.data = o.value;
+      c.click();
+      clearInterval(s);
+    }
+  }, 1000);
+
+
+})(document)
