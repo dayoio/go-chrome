f87c9e5bdd765955961f1cb2a5d15efea2d3aa50
Mino Zang <minoscc+dev@gmail.com>
2017-06-19 10:17:33 +0800

    block ws stream

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -6,6 +6,6 @@ chrome.webRequest.onBeforeRequest.addListener(
     urls: [
       'http://*/videos/other/*',
       'http://msg.71.am/cp2.gif*',
-      'http://static.iqiyi.com/js/player_v1/res/20170607/10442a1b82aa.js'
+      'http://static.iqiyi.com/js/player_v1/res/*'
     ]
   }, ['blocking'])
